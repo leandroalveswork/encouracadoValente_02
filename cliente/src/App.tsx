a49642cea8521dd4dc862daa96d9e0a16de4b9ce
Guilherme Schiavone <guilherme-schiavone@hotmail.com>
2022-10-16 16:42:54 -0300

    Implementando evoluções de layout e da lógica da partida

--- cliente/src/App.tsx
+++ cliente/src/App.tsx
@@ -2,35 +2,36 @@ import './App.css';
 import TelaJogo from './view/TelaJogo'
 import Home from './view/Home'
 import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  RouterProvider,
   Route,
   Routes,
 } from "react-router-dom";
 import Entrar from './view/auth/Entrar';
-import { GoogleOAuthProvider } from '@react-oauth/google';
+
 import IndexLoja from './view/loja/IndexLoja';
 import AdicionarTema from './view/loja/AdicionarTema';
 import DetalheTema from './view/loja/DetalheTema';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PreparacaoJogo from './view/PreparacaoJogo';
 
 function App() {
-      //TODO: Não disponibilizar chaves sensíveis no arquivo .env quando commitar o código . Sofrerão replace no processo de CI
+  //TODO: Não disponibilizar chaves sensíveis no arquivo .env quando commitar o código . Sofrerão replace no processo de CI
   return (
-    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}> 
+    <>
       <Header />
-      <Routes>
-        <Route path="/auth/entrar" element={<Entrar />} />,
-        <Route path="/loja" element={<IndexLoja />} />,
-        <Route path="/loja/adicionarTema" element={<AdicionarTema />} />,
-        <Route path="/loja/detalheTema" element={<DetalheTema />} />,
-        <Route path="/" element={<Home />} />,
-        <Route path="/game/:roomId" element={<TelaJogo backendUrl={`ws://${process.env.REACT_APP_url_do_servidor_backend as string}/room`} />} />,
-      </Routes>
+      <div className='app'>
+        <Routes>
+          <Route path="/auth/entrar" element={<Entrar />} />,
+          <Route path="/loja" element={<IndexLoja />} />,
+          <Route path="/loja/adicionarTema" element={<AdicionarTema />} />,
+          <Route path="/loja/detalheTema" element={<DetalheTema />} />,
+          <Route path="/" element={<Home />} />,
+          <Route path="/game/:roomId" element={<TelaJogo backendUrl={`ws://${process.env.REACT_APP_url_do_servidor_backend as string}/room`} />} />,
+          <Route path="/game/prepare" element={<PreparacaoJogo/> } />,
+        </Routes>
+      </div>
       <Footer />
-    </GoogleOAuthProvider>
+    </>
   )
 }
 
--- cliente/src/index.tsx
+++ cliente/src/index.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID as string}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </GoogleOAuthProvider>
 );
